c5a029b9a4f180eda8108295ac8966485b83a821
uday510 <admin@udayteja.com>
2023-05-12 21:59:05 +0530

    Add birthday msg

--- index.js
+++ index.js
@@ -23,8 +23,10 @@ app.get("/", async (req, res) => {
     req.headers["x-forwarded-for"]?.split(",").shift() ||
     req.socket?.remoteAddress;
 
-  res.send(`Welcome ${parseIp(req)}, it's me ${os.hostname()} 
-  with ❤️ from San Francisco, USA (West) - sfo1`);
+  // res.send(`Welcome ${parseIp(req)}, it's me ${os.hostname()} 
+  // with ❤️ from San Francisco, USA (West) - sfo1`);
+
+  res.send ('Wishing administrator a very happy birthday 🎈🎉🎂')
 });
 
 require("./routes")(app) // Initialize the route/s
