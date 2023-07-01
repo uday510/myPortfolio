07c93333393bfd3b3c9ced768e1987a1cc0cd8bc
Uday <admin@udayteja.com>
2023-07-02 04:40:59 +0530

    Add assets

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const bodyParser = require("body-parser");
 const dbConfig = require("./configs/db.config");
 const app = express(); // Initialize express instance
 const os = require("os");
+const PORT = 4000;
 
 console.clear(); // clear the console to remove previous logging
 
 // app.use(express.static(__dirname));
 
 // Logs time for every request
-function requestTime(req, res, next) {
-    process.stdout.write(`Request-Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} IST, ${req.method} ${req.url} \n`);
-    next();
-}
-app.use(requestTime); // logs request time
-app.use(bodyParser.json()); // used to parse the request and extract the information
-app.use(bodyParser.urlencoded({ extended: false }));
+// function requestTime(req, res, next) {
+//     process.stdout.write(`Request-Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} IST, ${req.method} ${req.url} \n`);
+//     next();
+// }
+// app.use(requestTime); // logs request time
+// app.use(bodyParser.json()); // used to parse the request and extract the information
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // for testing purposes
 app.get("/", async (req, res) => {
@@ -32,26 +33,24 @@ app.get("/", async (req, res) => {
   return res.send("Hello world");
 });
 
-require("./routes")(app) // Initialize the route/s
-// Connect to the Database
-mongoose
-  .connect(dbConfig.DB_URL, {
-    useNewUrlParser: true, // To avoid Deprecation Warning
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log(`Pinging Google Cloud Servers...`);
-    console.log(`Ping Successful`);
-    console.log(`Connecting to Atlas GCP Mumbai (asia-south1)...`);
-    console.log(`Connection established`);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+// require("./routes")(app) // Initialize the route/s
+// // Connect to the Database
+// mongoose
+//   .connect(dbConfig.DB_URL, {
+//     useNewUrlParser: true, // To avoid Deprecation Warning
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log(`Pinging Google Cloud Servers...`);
+//     console.log(`Ping Successful`);
+//     console.log(`Connecting to Atlas GCP Mumbai (asia-south1)...`);
+//     console.log(`Connection established`);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
 
 //Initialize the express server
-module.exports = app.listen((serverConfig.HOST, serverConfig.PORT), () => {
-  console.log(
-    `Admin Application Running on PORT: ${serverConfig.PORT}`
-  );
-});
+module.exports = app.listen(PORT, () => {
+  console.log(`Admin Application Running on PORT: ${PORT}`)
+})
