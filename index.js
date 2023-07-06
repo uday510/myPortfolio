f8c2e33eadbb33370bac8af835aec1c8534c6ffa
Uday <admin@udayteja.com>
2023-07-07 02:44:11 +0530

    Remove logs

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser");
 const dbConfig = require("./configs/db.config");
 const app = express(); // Initialize express instance
 const os = require("os");
-const PORT = 4000;
 
 console.clear(); // clear the console to remove previous logging
 
@@ -14,22 +13,24 @@ app.use(express.static(__dirname));
 
 // Logs time for every request
 function requestTime(req, res, next) {
-    process.stdout.write(`Request-Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} IST, ${req.method} ${req.url} \n`);
-    next();
+  process.stdout.write(`Request-Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} IST, ${req.method} ${req.url} \n`);
+  next();
 }
-app.use(requestTime); // logs request time
+// TODO -> DO NOT FUCKING USE CONSOLE LOG IN VERCEL PRODUCTION
+// app.use(requestTime); // logs request time
 app.use(bodyParser.json()); // used to parse the request and extract the information
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-// app.get("/", (req, res) => {
-  // return res.sendFile(path.join(__dirname + '/index.html'));
-//   const parseIp = (req) =>
-//     req.headers["x-forwarded-for"]?.split(",").shift() ||
-//     req.socket?.remoteAddress;
-//   res.send(`Welcome ${parseIp(req)}, it's me ${os.hostname()}
-//   with ❤️ from San Francisco, USA (West) - sfo1`);
-// });
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname + '/index.html'));
+  // const parseIp = (req) =>
+  //   req.headers["x-forwarded-for"]?.split(",").shift() ||
+  //   req.socket?.remoteAddress;
+  // res.send(`Welcome ${parseIp(req)}, it's me ${os.hostname()}
+  // with ❤️ from San Francisco, USA (West) - sfo1`);
+});
+
 require("./routes")(app) // Initialize the route/s
 
 
@@ -41,16 +42,8 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log(`Pinging Google Cloud Servers...`);
-    console.log(`Ping Successful`);
-    console.log(`Connecting to Atlas GCP Mumbai (asia-south1)...`);
-    console.log(`Connection established`);
+    app.listen(4000);
   })
   .catch((err) => {
-    console.log(err.message);
+    throw err;
   });
-
-//Initialize the express server
-module.exports = app.listen(PORT, () => {
-  console.log(`Admin Application Running on PORT: ${PORT}`)
-})
