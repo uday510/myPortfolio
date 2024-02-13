37aa52ff6311e43f7e38e188b010b7fe33cc32ec
Uday <budayteja009@gmail.com>
2024-02-13 11:52:50 +0530

    Add Testimonials

--- js/title.js
+++ js/title.js
@@ -1,17 +1,9 @@
 function updateTitle() {
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-
-  // Add leading zero if the values are less than 10
-  hours = (hours < 10 ? "0" : "") + hours;
-  minutes = (minutes < 10 ? "0" : "") + minutes;
-  seconds = (seconds < 10 ? "0" : "") + seconds;
-
-  var formattedTime = hours + ":" + minutes + ":" + seconds;
-
-  document.title = formattedTime;
+  // show utc time with milliseconds
+  var date = new Date();
+  var time = date.toISOString().split('T')[1].split('.')[0];
+  
+  document.title = time;
 }
 
 // Update the title every second
